b84e812b37867eaf156c509938ae0389ff266324
Ellis John <ellisjohn@zipcodes-MacBook-Pro-chuck.local>
2021-12-18 13:52:15 -0500

    lab

---
+++ src/main/java/models/Cars.java
@@ -0,0 +1,72 @@
+package models;
+
+public class Cars {
+
+    private Integer id;
+    private String make;
+    private String model;
+    private Integer year;
+    private String color;
+
+    public Cars(Integer id, String make, String model, Integer year, String color) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public Cars() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cars{" +
+                "id=" + id +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
